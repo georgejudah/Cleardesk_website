6cd477c868a4f0113dfb16e359cdac35dd315767
George <jgeorg528@apac.comcast.com>
2023-10-10 15:57:41 +0530

    ++improved UI ++included proper file for download

--- app.py
+++ app.py
@@ -1,38 +1,27 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, render_template, send_file
 import zipfile
 import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/download-zip')
 def download_zip():
-    # Create a temporary directory for storing files to be zipped
-    temp_dir = 'downloads/temp'
-    os.makedirs(temp_dir, exist_ok=True)
+    # Path to the zipped folder
+    zip_folder_path = 'clear_desk-win32-x64.zip'
 
-    # Create some example files (you can replace these with your own)
-    for i in range(3):
-        with open(f'{temp_dir}/file{i}.txt', 'w') as file:
-            file.write(f'This is file {i}')
+    # Check if the zipped folder exists
+    if not os.path.exists(zip_folder_path):
+        return "Zipped folder not found", 404
 
-    # Create a zip file containing the files in the temporary directory
-    zip_filename = 'myfiles.zip'
-    with zipfile.ZipFile(os.path.join('downloads', zip_filename), 'w') as zipf:
-        for root, _, files in os.walk(temp_dir):
-            for file in files:
-                zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), temp_dir))
+    # Send the zipped folder to the user
+    return send_file(zip_folder_path, as_attachment=True)
 
-    # Remove the temporary directory
-    for file in os.listdir(temp_dir):
-        os.remove(os.path.join(temp_dir, file))
-    os.rmdir(temp_dir)
-
-    # Send the zip file to the user
-    return send_from_directory('downloads', zip_filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
